969042bec893aeb57e3af0e67ceb446d224f2181
TheDoctor314 <darsh.rajan95@gmail.com>
2021-05-04 16:29:14 +0530

    Implement Display trait for required structs

--- src/error.rs
+++ src/error.rs
@@ -3,3 +3,11 @@ pub(crate) enum RloxError {
     // Returned if scanner encounters an error
     Lexical(usize, String, String),
 }
+
+impl std::fmt::Display for RloxError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            RloxError::Lexical(ref line, ref msg, ref what) => write!(f, "Lexical Error [line {}] {}: {:?}", line, msg, what),
+        }
+    }
+}
--- src/tokens.rs
+++ src/tokens.rs
@@ -82,7 +82,7 @@ impl fmt::Display for Token {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         write!(
             f,
-            "{:?} {} {:?}",
+            "{{{:?} {} {:?}}}",
             self.token_type, self.lexeme, self.literal
         )
     }
@@ -106,4 +106,61 @@ pub(crate) enum Literal {
     Boolean(bool),
     Number(f64),
     String(String),
+}
+
+impl std::fmt::Display for Literal {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            Literal::Nil => write!(f, "nil"),
+            Literal::Boolean(b) => write!(f, "{}", b),
+            Literal::Number(n) => write!(f, "{}", n),
+            Literal::String(ref s) => write!(f, "{}", s),
+        }
+    }
+}
+
+impl std::fmt::Display for TokenType {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            TokenType::LParen => write!(f, "L_PAREN"),
+            TokenType::RParen => write!(f, "R_PAREN"),
+            TokenType::LBrace => write!(f, "L_BRACE"),
+            TokenType::RBrace => write!(f, "R_BRACE"),
+            TokenType::Comma => write!(f, "COMMA"),
+            TokenType::Dot => write!(f, "DOT"),
+            TokenType::Minus => write!(f, "MINUS"),
+            TokenType::Plus => write!(f, "PLUS"),
+            TokenType::SemiColon => write!(f, "SEMICOLON"),
+            TokenType::Slash => write!(f, "SLASH"),
+            TokenType::Star => write!(f, "STAR"),
+            TokenType::Bang => write!(f, "BANG"),
+            TokenType::Equal => write!(f, "EQUAL"),
+            TokenType::BangEqual => write!(f, "BANG_EQ"),
+            TokenType::EqualEqual => write!(f, "EQ_EQ"),
+            TokenType::Greater => write!(f, "GREATER"),
+            TokenType::GreaterEqual => write!(f, "GREATER_EQ"),
+            TokenType::Less => write!(f, "LESS"),
+            TokenType::LessEqual => write!(f, "LESS_EQ"),
+            TokenType::Ident => write!(f, "IDENT"),
+            TokenType::StringLiteral => write!(f, "STRING"),
+            TokenType::Number => write!(f, "NUM"),
+            TokenType::And => write!(f, "AND"),
+            TokenType::Class => write!(f, "CLASS"),
+            TokenType::Else => write!(f, "ELSE"),
+            TokenType::False => write!(f, "FALSE"),
+            TokenType::Fun => write!(f, "FUN"),
+            TokenType::For => write!(f, "FOR"),
+            TokenType::If => write!(f, "IF"),
+            TokenType::Nil => write!(f, "NIL"),
+            TokenType::Or => write!(f, "OR"),
+            TokenType::Print => write!(f, "PRINT"),
+            TokenType::Return => write!(f, "RETURN"),
+            TokenType::Super => write!(f, "SUPER"),
+            TokenType::This => write!(f, "THIS"),
+            TokenType::True => write!(f, "TRUE"),
+            TokenType::Var => write!(f, "VAR"),
+            TokenType::While => write!(f, "WHILE"),
+            TokenType::Eof => write!(f, "EOF"),
+        }
+    }
 }
