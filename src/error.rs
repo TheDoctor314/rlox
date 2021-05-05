0e6dd7daec298576dc300fe2b3aea1958a1e6c50
TheDoctor314 <darsh.rajan95@gmail.com>
2021-05-05 21:21:23 +0530

    Add Parse error

--- src/error.rs
+++ src/error.rs
@@ -2,13 +2,19 @@
 pub(crate) enum RloxError {
     // Returned if scanner encounters an error
     Lexical(usize, String, String),
+    Parse(usize, String, String),
 }
+
+pub(crate) type Result<T> = std::result::Result<T, RloxError>;
 
 impl std::fmt::Display for RloxError {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
             RloxError::Lexical(ref line, ref msg, ref what) => {
                 write!(f, "Lexical Error [line {}] {}: {:?}", line, msg, what)
+            }
+            RloxError::Parse(ref line, ref msg, ref near) => {
+                write!(f, "Parse Error [line {}] {}: {:?}", line, msg, near)
             }
         }
     }
