b3860f5f909ad9aca3c162d7988af565e48ebe22
TheDoctor314 <darsh.rajan95@gmail.com>
2021-05-11 16:16:43 +0530

    Add test runner
    run_master() function takes input(&str).
    It runs the corresponding file in input/ and compares the
    output with the file in output/.
    
    test_case!() is a macro which takes name of test and the input file.
    Run tests using this macro if you like.

---
+++ tests/run.rs
@@ -0,0 +1,32 @@
+use std::{path::PathBuf, process::Command};
+
+const INPUT_DIR: &str = "tests/input";
+const OUTPUT_DIR: &str = "tests/output";
+
+macro_rules! test_case {
+    ($name:ident, $input:expr) => {
+        #[test]
+        fn $name() { run_master($input) }
+    };
+}
+
+fn run_master(input: &str) {
+    let in_file: PathBuf = [INPUT_DIR, input].iter().collect();
+
+    let output = input.to_string() + &".out";
+    let out_file: PathBuf = [OUTPUT_DIR, &output].iter().collect();
+
+    let expected = std::fs::read_to_string(&out_file).expect("Failed to read file");
+
+    let actual = Command::new("cargo").args(&["run", in_file.as_path().to_str().unwrap()]).output().expect("Failed to execute process").stdout;
+    let actual = String::from_utf8(actual).expect("Failed to convert to string");
+
+    assert_eq!(&expected, &actual);
+}
+
+test_case!(brk, "break.lox");
+test_case!(expr, "expr.lox");
+test_case!(counter, "counter.lox");
+test_case!(function, "function.lox");
+test_case!(lambda, "lambda.lox");
+test_case!(loops, "loops.lox");
