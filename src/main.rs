1f3970eacb7dc4d774db874884f3d32592023a4a
TheDoctor314 <darsh.rajan95@gmail.com>
2021-05-12 17:45:08 +0530

    Move functionality from main.rs to runner.rs

--- src/main.rs
+++ src/main.rs
@@ -8,73 +8,29 @@ mod functions;
 mod interpreter;
 mod object;
 mod parser;
+mod runner;
 mod scanner;
 mod stmt;
 mod tokens;
 
-use std::path::Path;
-
-use interpreter::Interpreter;
-
 fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let mut run = runner::Runner {};
+
     let args = std::env::args().collect::<Vec<_>>();
-    match args.len() {
-        1 => run_prompt()?,
-        2 => run_file(&args[1])?,
-        _ => {
-            eprintln!("Usage: rlox [script]");
-            std::process::exit(64);
+    let res = {
+        match args.len() {
+            1 => run.prompt(),
+            2 => run.file(&args[1]),
+            _ => {
+                eprintln!("Usage: rlox [script]");
+                std::process::exit(64);
+            }
         }
+    };
+
+    if let Err(e) = res {
+        eprintln!("{}", e);
+        std::process::exit(1);
     }
-    Ok(())
-}
-
-fn run_file<P>(file: &P) -> Result<(), Box<dyn std::error::Error>>
-where
-    P: AsRef<Path>,
-{
-    let src = std::fs::read_to_string(file)?;
-    let mut i = Interpreter::new(false);
-    run(&src, &mut i)?;
-
-    Ok(())
-}
-
-fn run_prompt() -> Result<(), Box<dyn std::error::Error>> {
-    use rustyline::{error, Editor};
-    let mut reader = Editor::<()>::new();
-    let mut i = Interpreter::new(true);
-
-    loop {
-        let line = reader.readline(">> ");
-        match line {
-            Ok(line) => run(&line, &mut i)?,
-            Err(error::ReadlineError::Interrupted) => break,
-            Err(error::ReadlineError::Eof) => break,
-            Err(err) => return Err(err.into()),
-        }
-    }
-
-    Ok(())
-}
-fn run(src: &str, i: &mut Interpreter) -> Result<(), Box<dyn std::error::Error>> {
-    let scan = scanner::Scanner::new(src.chars());
-    //let tokens_vec: Vec<tokens::Token> = scan.map(|x| x.unwrap()).collect();
-    //println!(
-    //    "[{}]",
-    //    tokens_vec
-    //        .iter()
-    //        .fold(String::new(), |acc, token| acc + &token.to_string() + ", ")
-    //);
-
-    let parser = parser::Parser::new(scan);
-
-    for stmt in parser {
-        match stmt {
-            Err(e) => eprintln!("{}", e),
-            Ok(stmt) => i.interpret(&stmt).unwrap(),
-        }
-    }
-
     Ok(())
 }
---
+++ src/runner.rs
@@ -0,0 +1,57 @@
+use std::path::Path;
+
+use crate::{
+    error::Result, interpreter::Interpreter, parser::StmtIterator, scanner::TokenIterator,
+};
+
+pub struct Runner;
+
+impl Runner {
+    pub(crate) fn file<P>(&mut self, f: &P) -> Result<()>
+    where
+        P: AsRef<Path>,
+    {
+        let src = std::fs::read_to_string(f)?;
+
+        let mut i = Interpreter::new(false);
+
+        self.run(&mut i, &src)
+    }
+
+    pub(crate) fn prompt(&mut self) -> Result<()> {
+        use rustyline::error::ReadlineError;
+        use rustyline::Editor;
+
+        let mut reader = Editor::<()>::new();
+        let mut i = Interpreter::new(true);
+
+        loop {
+            let line = reader.readline(">> ");
+            match line {
+                Err(ReadlineError::Interrupted) => break,
+                Err(ReadlineError::Eof) => break,
+                Err(e) => {
+                    eprintln!("{}", e);
+                }
+                Ok(line) => {
+                    if let Err(e) = self.run(&mut i, &line) {
+                        eprintln!("{}", e);
+                    }
+                }
+            }
+        }
+
+        Ok(())
+    }
+
+    pub(crate) fn run(&mut self, i: &mut Interpreter, src: &str) -> Result<()> {
+        for res in src.chars().tokens().statements() {
+            match res {
+                Err(e) => eprintln!("{}", e),
+                Ok(stmt) => i.interpret(&stmt)?,
+            }
+        }
+
+        Ok(())
+    }
+}
